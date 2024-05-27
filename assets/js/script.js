530910ef2b62faebacaff90323e83f384a144ef6
Pritam Sur <sur.pritam9878@gmail.com>
2024-05-27 19:39:31 -0400

    Add top stories functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,27 @@ $( function() {
       source: ticker_list
     });
   } );
+
+// Get top news data for stocks
+const url = 'https://seeking-alpha.p.rapidapi.com/news/v2/list-by-symbol?id=aapl&size=5&number=1';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'Your API Key',
+		'X-RapidAPI-Host': 'seeking-alpha.p.rapidapi.com'
+	}
+};
+
+fetch(url, options)
+.then(function(response) {
+	return response.json();
+}).then(function(data){
+	const newsContainer = document.querySelector('.top-stories-container');
+	const dataArr = data.data;
+	dataArr.forEach(news => {
+		console.log(news);
+		const newsparagraph = document.createElement('p');
+		newsparagraph.textContent = news.attributes.title;
+		newsContainer.appendChild(newsparagraph);
+	});
+});
